fd87345bd0bb0f4af69735c2a810250fb37bc5a4
tn49gadgetz49-gif <tn49gadgetz49@gmail.com>
2026-08-10 08:34:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,67 @@
+const CACHE_NAME = "tn49-gadgetz-pwa-v1";
 
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./tn49-gadgetz-icon-192.png",
+  "./tn49-gadgetz-icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request)
+          .then(response => {
+
+            if (
+              !response ||
+              response.status !== 200 ||
+              response.type === "opaque"
+            ) {
+              return response;
+            }
+
+            const responseClone = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseClone);
+              });
+
+            return response;
+          })
+          .catch(() => {
+            return caches.match("./index.html");
+          });
+      })
+  );
+});
